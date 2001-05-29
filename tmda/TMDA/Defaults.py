5bb11db4551d5170d1237263e7aec10308cf7627
jasonrm <jasonrm@2baf9af7-fe1d-0410-8d70-c10643b1d81c>
2001-05-29 15:32:30 +0000

    Begin 0.16 development.
    git-svn-id: https://tmda.svn.sourceforge.net/svnroot/tmda/trunk@81 2baf9af7-fe1d-0410-8d70-c10643b1d81c

--- tmda/TMDA/Defaults.py
+++ tmda/TMDA/Defaults.py
@@ -16,7 +16,7 @@ import Util
 # General system-wide defaults
 ##############################
 
-TMDA_VERSION = "0.15"
+TMDA_VERSION = "0.16"
 TMDA_HOMEPAGE = "<http://tmda.sourceforge.net/>"
 
 # Exit codes: everything except 0, 99 and 100 are soft errors.
