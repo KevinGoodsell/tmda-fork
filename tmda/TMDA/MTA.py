56264006ef31ded9c7d9db48f3cf1c0388330ac8
jasonrm <jasonrm@2baf9af7-fe1d-0410-8d70-c10643b1d81c>
2001-10-15 16:01:26 +0000

    Fix a bug in the Postfix deliver method where the exit code from LOCAL_DELIVERY_AGENT was not being propagated to the MTA.  (e.g, tmda-filter returns 0 if procmail returns 77.)  The exit status of the pipe's close() function has the signal the command died from in the low byte (if any) and the exit status of the command in the high byte.
    The fix is to do some more processing on the exitcode before passing
    it to sys.exit.
    
    What I've done here is check for unexpected death by signal in the lda
    and deferred when that happens.  I tested this by setting
    LOCAL_DELIVERY_AGENT to a small C program that segfaults.  It seems to
    work.
    
    Otherwise, I examine the exit status.  If it is one postfix will
    understand (64 to 78 inclusive), then we honor it and return that
    value.
    
    Otherwise we defer.  I think it is useful to defer when the exit code is
    not 64 to 78 inclusive.  It will save people's butts when their LDA
    isn't acting correctly.  maildrop and postfix both seem to return 75
    when they encounter a random error, but other home-grown LDAs won't.
    
    Patch contributed by Matt Armstrong.
    
    
    git-svn-id: https://tmda.svn.sourceforge.net/svnroot/tmda/trunk@270 2baf9af7-fe1d-0410-8d70-c10643b1d81c

--- tmda/TMDA/MTA.py
+++ tmda/TMDA/MTA.py
@@ -25,13 +25,17 @@ class Postfix(MTA):
         self.EX_OK = 0                  # successful termination; exit
         self.EX_STOP = None             # Postfix doesn't have such an exit code
         self.EX_TEMPFAIL = 75           # temporary failure; defer delivery
+        
     # Define the four states of a message.
     def bounce(self):
         sys.exit(self.EX_HARD)
+
     def defer(self):
         sys.exit(self.EX_TEMPFAIL)
+
     def stop(self):
         sys.exit(self.EX_OK)
+
     def deliver(self, message=None):
         self.local_delivery_agent = Defaults.LOCAL_DELIVERY_AGENT
         # Make sure the LDA actually exists first.
@@ -42,7 +46,21 @@ class Postfix(MTA):
         else:
             self.__pipeline = os.popen(self.local_delivery_agent, 'w')
             self.__pipeline.write(message)
-            sys.exit(self.__pipeline.close())
+            exitcode = self.__pipeline.close()
+            if not exitcode:
+                exitcode = 0
+            signal = exitcode & 0x7f
+            exitval = exitcode >> 8
+            if signal:
+                print "LOCAL_DELIVERY_AGENT died by signal", signal
+                self.defer()
+            elif exitval == 0:
+                self.stop()
+            elif exitval >= 64 and exitval <= 78:
+                sys.exit(exitval)
+            else:
+                print "LOCAL_DELIVERY_AGENT unknown exit value", exitval
+                self.defer()
 
 
 class Qmail(MTA):
@@ -54,13 +72,17 @@ class Qmail(MTA):
         self.EX_OK = 0                  # success; process next instruction
         self.EX_STOP = 99               # success; ignore further instructions
         self.EX_TEMPFAIL = 111          # soft error; defer delivery
+
     # Define the four states of a message.
     def bounce(self):
         sys.exit(self.EX_HARD)
+
     def defer(self):
         sys.exit(self.EX_TEMPFAIL)
+
     def stop(self):
         sys.exit(self.EX_STOP)
+
     def deliver(self, message=None):
         sys.exit(self.EX_OK)
 
