f54d95d14f1a4c39437aa320cdeb62676eb88f4b
jasonrm <jasonrm@2baf9af7-fe1d-0410-8d70-c10643b1d81c>
2001-10-22 23:01:41 +0000

    A more lengthly historical overview has been added in its own file (history.html).  The previous, brief historical information was removed from index.html.
    git-svn-id: https://tmda.svn.sourceforge.net/svnroot/tmda/trunk@302 2baf9af7-fe1d-0410-8d70-c10643b1d81c

--- tmda/htdocs/overview-links.h
+++ tmda/htdocs/overview-links.h
@@ -1,6 +1,7 @@
 <!-- -*- html -*- -->
 <h3>Overview</h3>
 <li><a href="index.html">Introduction</a>
+<li><a href="history.html">History</a>
 <li><a href="features.html">Features</a>
 <li><a href="results.html">Results &amp; Testimonials</a>
 <li><a href="inuse.html">TMDA In Use</a>
