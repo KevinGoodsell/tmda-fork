15ac87b379be4b09f99dd8575cf974dd3e019eea
jasonrm <jasonrm@2baf9af7-fe1d-0410-8d70-c10643b1d81c>
2001-11-02 00:26:35 +0000

    Reorganize and also document the Exim support.
    git-svn-id: https://tmda.svn.sourceforge.net/svnroot/tmda/trunk@323 2baf9af7-fe1d-0410-8d70-c10643b1d81c

--- tmda/htdocs/config-links.h
+++ tmda/htdocs/config-links.h
@@ -1,5 +1,6 @@
 <!-- -*- html -*- -->
 <h3>Configuration</h3>
 <li><a href="config.html">Overview</a>
+<li><a href="config-pre.html">Pre-Configuration</a>
 <li><a href="config-server.html">Server Configuration</a>
 <li><a href="config-client.html">Client Configuration</a>
