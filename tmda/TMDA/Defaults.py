cdbb17852a8117e10f5b688f1523a0debf7542c1
jasonrm <jasonrm@2baf9af7-fe1d-0410-8d70-c10643b1d81c>
2001-12-04 17:59:22 +0000

    Begin 0.43 development.
    git-svn-id: https://tmda.svn.sourceforge.net/svnroot/tmda/trunk@386 2baf9af7-fe1d-0410-8d70-c10643b1d81c

--- tmda/TMDA/Defaults.py
+++ tmda/TMDA/Defaults.py
@@ -15,7 +15,7 @@ import Util
 # General system-wide defaults
 ##############################
 
-TMDA_VERSION = "0.42"
+TMDA_VERSION = "0.43"
 TMDA_HOMEPAGE = "<http://tmda.sf.net/>"
 
 PYTHON_VERSION = string.split(sys.version)[0]
--- tmda/contrib/tmda.spec
+++ tmda/contrib/tmda.spec
@@ -1,5 +1,5 @@
 %define name tmda
-%define version 0.42
+%define version 0.43
 %define release 1
 
 Summary: Tagged Message Delivery Agent
