d2f39dd332f597f9d8523c01dc2c8b725f340a90
jasonrm <jasonrm@2baf9af7-fe1d-0410-8d70-c10643b1d81c>
2002-01-08 21:01:51 +0000

    Reorganize the support information.
    - Add a "Troubleshooting" page for procedures to follow when
    debugging/reporting problems.
    
    - Remove the redundant lists.html -- this info is available
    from the Mailman pages.
    
    
    git-svn-id: https://tmda.svn.sourceforge.net/svnroot/tmda/trunk@464 2baf9af7-fe1d-0410-8d70-c10643b1d81c

--- tmda/htdocs/support-links.h
+++ tmda/htdocs/support-links.h
@@ -1,6 +1,8 @@
 <!-- -*- html -*- -->
 <h3>Support</h3>
-<li><A href="faq.html">FAQ</a>
-<li><a href="lists.html">Mailing Lists</a>
+<li><a href="trouble.html">Troubleshooting</a>
+<li><a href="faq.html">FAQ</a>
 <li><a href="bugs.html">Bugs &amp; Patches</a>
+<li><a href="http://libertine.org/lists/listinfo/" TARGET="Resource Window">Mailing Lists</a>
+<li><a href="http://mla.libertine.org/" TARGET="Resource Window">List Archive</a> 
 <li><a href="resources.html">External Resources</a>
