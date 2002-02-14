73518721ed2b18b3467a178147af1047611609be
jasonrm <jasonrm@2baf9af7-fe1d-0410-8d70-c10643b1d81c>
2002-02-14 00:43:07 +0000

    Cosmetic enhancement.  Separate PYTHON and PLATFORM with `on' rather than `;'.
    This is more Pythonic, as sys.version and sys.platform are separated
    this way.  e.g,
    
    % python
    Python 2.2 (#1, Dec 21 2001, 15:19:53) [C] on irix646
    Type "help", "copyright", "credits" or "license" for more information.
    >>>
    
    
    git-svn-id: https://tmda.svn.sourceforge.net/svnroot/tmda/trunk@538 2baf9af7-fe1d-0410-8d70-c10643b1d81c

--- tmda/TMDA/Version.py
+++ tmda/TMDA/Version.py
@@ -16,5 +16,5 @@ PYTHON = sys.version.split()[0]
 PLATFORM = sys.platform
 
 # A summary of all the version identifiers.  e.g,
-# TMDA/0.46 (Python 2.2; freebsd4)
-ALL = "TMDA/%s (Python %s; %s)" % (TMDA, PYTHON, PLATFORM)
+# TMDA/0.46 (Python 2.2 on freebsd4)
+ALL = "TMDA/%s (Python %s on %s)" % (TMDA, PYTHON, PLATFORM)
