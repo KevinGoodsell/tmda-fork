115753b553dfe3afb77a34cdb84d43a0dc0f0254
jasonrm <jasonrm@2baf9af7-fe1d-0410-8d70-c10643b1d81c>
2002-07-22 19:55:35 +0000

    Swing the mailing lists over to tmda.net.
    git-svn-id: https://tmda.svn.sourceforge.net/svnroot/tmda/trunk@803 2baf9af7-fe1d-0410-8d70-c10643b1d81c

--- tmda/htdocs/support-links.h
+++ tmda/htdocs/support-links.h
@@ -3,6 +3,6 @@
 <li><a href="trouble.html">Troubleshooting</a>
 <li><a href="http://tmda.net/faq.cgi" TARGET="Resource Window">FAQ</a>
 <li><a href="bugs.html">Bugs &amp; Patches</a>
-<li><a href="http://libertine.org/lists/listinfo/" TARGET="Resource Window">Mailing Lists</a>
+<li><a href="http://tmda.net/lists/listinfo/" TARGET="Resource Window">Mailing Lists</a>
 <li><a href="http://mla.libertine.org/" TARGET="Resource Window">List Archive</a> 
 <li><a href="resources.html">External Resources</a>
