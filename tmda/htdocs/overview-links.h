d60a9a75e52b5516c1d43773387b182f99463959
jasonrm <jasonrm@2baf9af7-fe1d-0410-8d70-c10643b1d81c>
2002-08-07 20:50:41 +0000

    Add a "Press Coverage" page.
    git-svn-id: https://tmda.svn.sourceforge.net/svnroot/tmda/trunk@828 2baf9af7-fe1d-0410-8d70-c10643b1d81c

--- tmda/htdocs/overview-links.h
+++ tmda/htdocs/overview-links.h
@@ -5,3 +5,4 @@
 <li><a href="features.html">Features</a>
 <li><a href="results.html">Results &amp; Testimonials</a>
 <li><a href="inuse.html">TMDA In Use</a>
+<li><a href="press.html">Press Coverage</a>
