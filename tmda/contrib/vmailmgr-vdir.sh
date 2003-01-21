aed8b6fbaba4eb2c533f96946b337c47aaf7e6fb
tlegant <tlegant@2baf9af7-fe1d-0410-8d70-c10643b1d81c>
2003-01-21 05:31:35 +0000

    Added two sample scripts to find virtual users' home directories.
    git-svn-id: https://tmda.svn.sourceforge.net/svnroot/tmda/trunk@1070 2baf9af7-fe1d-0410-8d70-c10643b1d81c

---
+++ tmda/contrib/vmailmgr-vdir.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# Prints the virtual email user's home directory.
+#
+
+# Expects a virtual username and the virtual domain as parameters.
+# The username should be just the username portion and not the entire
+# email address, e.g., if the user usually logs in as
+# 'joe@example.com' or 'joe%example.com', this script should be run as
+# the system user in charge of 'example.com' (see the 'prepend' in
+# /var/qmail/control/virtualdomains) and given the parameters 'joe'
+# and 'example.com'.
+#
+# $ vmailmgr-vdir.sh joe example.com
+#
+# NOTE: At this point, the domain parameter ($2) is not used.
+
+# Change the following paths if the sed program or VMailMgr's listvdomain
+# program are in different locations than given here.
+LISTVDOMAIN=/usr/local/bin/listvdomain
+SED=/usr/bin/sed
+
+# Set IFS to a newline (no space, no tab)
+IFS='
+'
+set `${LISTVDOMAIN} $1`
+# Reset IFS to a space, tab, newline.
+IFS=' 	
+'
+set $2
+
+# $2, now the user's home directory, is relative to $HOME and looks like this:
+#
+# ./users/<username>
+#
+# Strip off the leading dot, but leave the (now leading) '/'.
+homedir=`echo $2 | ${SED} 's/^\.//'`
+
+# No separating '/' necessary because 'homedir' begins with '/'.
+echo ${HOME}${homedir}
---
+++ tmda/contrib/vpopmail-vdir.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Prints the virtual email user's home directory.
+#
+
+# Expects a virtual username and the virtual domain as parameters.
+# The username should be just the username portion and not the entire
+# email address, e.g., if the user usually logs in as
+# 'joe@example.com' or 'joe%example.com', this script should be run as
+# the vpopmail user and given the parameters 'joe' and 'example.com'.
+#
+# $ vpopmail-vdir.sh joe example.com
+
+# Change the following paths if VPopMail's vuserinfo program is in a
+# different location than given here.
+VUSERINFO=/home/vpopmail/bin/vuserinfo
+
+if [ -z "$2" ]
+then
+    vuser="$1"
+else
+    vuser="$1@$2"
+fi
+
+${VUSERINFO} -d $vuser
