53a8a6446f31581eb8cca983bb3dac8db8707256
gre7g <gre7g@2baf9af7-fe1d-0410-8d70-c10643b1d81c>
2003-04-14 21:58:39 +0000

    Links must include SID.
    git-svn-id: https://tmda.svn.sourceforge.net/svnroot/tmda/trunk@1288 2baf9af7-fe1d-0410-8d70-c10643b1d81c

--- tmda-cgi/display/themes/Blue/template/links.h
+++ tmda-cgi/display/themes/Blue/template/links.h
@@ -1,21 +1,21 @@
 <!-- -*- html -*- -->
 <h3>Addresses</h3>
-<li><a href="%(Script)s?cmd=gen_addr">Generate Dynamic/Return Address</a>
-<li><a href="%(Script)s?cmd=test_addr">Test Dynamic Address</a>
+<li><a href="%(Script)s?cmd=gen_addr&SID=%(SID)s">Generate Dynamic/Return Address</a>
+<li><a href="%(Script)s?cmd=test_addr&SID=%(SID)s">Test Dynamic Address</a>
 <h3>Pending</h3>
-<li><a href="%(Script)s?cmd=pending">Pending E-mails</a>
-<li><a href="%(Script)s?cmd=view">E-mail</a>
-<li><a href="%(Script)s?cmd=autocleanup">Auto-cleanup</a>
+<li><a href="%(Script)s?cmd=pending&SID=%(SID)s">Pending E-mails</a>
+<li><a href="%(Script)s?cmd=view&SID=%(SID)s">E-mail</a>
+<li><a href="%(Script)s?cmd=autocleanup&SID=%(SID)s">Auto-cleanup</a>
 <h3>Filters</h3>
-<li><a href="%(Script)s?cmd=incoming">Incoming Filter</a>
-<li><a href="%(Script)s?cmd=outgoing">Outgoing Filter</a>
-<li><a href="%(Script)s?cmd=test_filt">Test Filters</a>
+<li><a href="%(Script)s?cmd=incoming&SID=%(SID)s">Incoming Filter</a>
+<li><a href="%(Script)s?cmd=outgoing&SID=%(SID)s">Outgoing Filter</a>
+<li><a href="%(Script)s?cmd=test_filt&SID=%(SID)s">Test Filters</a>
 <h3>Lists</h3>
-<li><a href="%(Script)s?cmd=editlist">%(Lists)s</a>
+<li><a href="%(Script)s?cmd=editlist&SID=%(SID)s">%(Lists)s</a>
 <h3>Settings</h3>
-<li><a href="%(Script)s?cmd=localconfig">Local Config</a>
-<li><a href="%(Script)s?cmd=globalconfig">Global Config</a>
-<li><a href="%(Script)s?cmd=theme">Theme</a>
-<li><a href="%(Script)s?cmd=password">Change Password</a>
+<li><a href="%(Script)s?cmd=localconfig&SID=%(SID)s">Local Config</a>
+<li><a href="%(Script)s?cmd=globalconfig&SID=%(SID)s">Global Config</a>
+<li><a href="%(Script)s?cmd=theme&SID=%(SID)s">Theme</a>
+<li><a href="%(Script)s?cmd=password&SID=%(SID)s">Change Password</a>
 <h3>Logout</h3>
-<li><a href="%(Script)s?cmd=logout"></a>
+<li><a href="%(Script)s?cmd=logout&SID=%(SID)s"></a>
