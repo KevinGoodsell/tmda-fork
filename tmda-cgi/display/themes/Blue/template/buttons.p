88fe50b21f62848eaa9654ea87be5478d039edc0
gre7g <gre7g@2baf9af7-fe1d-0410-8d70-c10643b1d81c>
2003-04-14 04:29:33 +0000

    Templates for the blue theme.
    git-svn-id: https://tmda.svn.sourceforge.net/svnroot/tmda/trunk@1265 2baf9af7-fe1d-0410-8d70-c10643b1d81c

---
+++ tmda-cgi/display/themes/Blue/template/buttons.p
@@ -0,0 +1,54 @@
+(dp0
+S'topics'
+p1
+(lp2
+S'Addresses'
+p3
+aS'Pending'
+p4
+aS'Filters'
+p5
+aS'Lists'
+p6
+aS'Settings'
+p7
+aS'Logout'
+p8
+asS'subtopics'
+p9
+(lp10
+S'   E-mail'
+p11
+aS'Generate Dynamic/Return Address'
+p12
+aS'Test Dynamic Address'
+p13
+aS'Pending E-mails'
+p14
+aS'Auto-cleanup'
+p15
+aS'Incoming Filter'
+p16
+aS'Outgoing Filter'
+p17
+aS'Test Filters'
+p18
+aS'Local Config'
+p19
+aS'Global Config'
+p20
+aS'Theme'
+p21
+aS'Change Password'
+p22
+aS'whitelist'
+p23
+aS'blacklist'
+p24
+aS'confirmed'
+p25
+aS'revoked'
+p26
+aS'other'
+p27
+as.
---
+++ tmda-cgi/display/themes/Blue/template/links.h
@@ -0,0 +1,21 @@
+<!-- -*- html -*- -->
+<h3>Addresses</h3>
+<li><a href="%(Script)s?cmd=gen_addr">Generate Dynamic/Return Address</a>
+<li><a href="%(Script)s?cmd=test_addr">Test Dynamic Address</a>
+<h3>Pending</h3>
+<li><a href="%(Script)s?cmd=pending">Pending E-mails</a>
+<li><a href="%(Script)s?cmd=view">E-mail</a>
+<li><a href="%(Script)s?cmd=autocleanup">Auto-cleanup</a>
+<h3>Filters</h3>
+<li><a href="%(Script)s?cmd=incoming">Incoming Filter</a>
+<li><a href="%(Script)s?cmd=outgoing">Outgoing Filter</a>
+<li><a href="%(Script)s?cmd=test_filt">Test Filters</a>
+<h3>Lists</h3>
+<li><a href="%(Script)s?cmd=editlist">%(Lists)s</a>
+<h3>Settings</h3>
+<li><a href="%(Script)s?cmd=localconfig">Local Config</a>
+<li><a href="%(Script)s?cmd=globalconfig">Global Config</a>
+<li><a href="%(Script)s?cmd=theme">Theme</a>
+<li><a href="%(Script)s?cmd=password">Change Password</a>
+<h3>Logout</h3>
+<li><a href="%(Script)s?cmd=logout"></a>
