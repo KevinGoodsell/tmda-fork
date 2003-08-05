bf98e4ccabfe1c052b72c07385c4b7037dacfd33
jasonrm <jasonrm@2baf9af7-fe1d-0410-8d70-c10643b1d81c>
2003-08-05 20:19:42 +0000

    Refactor the index page.  Move lengthly challenge/response description onto its own page to make the home page simpler and more concise.
    git-svn-id: https://tmda.svn.sourceforge.net/svnroot/tmda/trunk@1679 2baf9af7-fe1d-0410-8d70-c10643b1d81c

--- tmda/htdocs/links.h
+++ tmda/htdocs/links.h
@@ -3,6 +3,7 @@
 <li><a href="index.html">Introduction</a>
 <li><a href="history.html">History</a>
 <li><a href="features.html">Features</a>
+<li><a href="challengeresponse.html">Challenge/Response</a>
 <li><a href="results.html">Results &amp; Testimonials</a>
 <li><a href="inuse.html">TMDA In Use</a>
 <li><a href="press.html">Press Coverage</a><!-- -*- html -*- -->
