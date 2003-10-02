8110926a970acb58df6fc1ce1d23d007771c2689
jasonrm <jasonrm@2baf9af7-fe1d-0410-8d70-c10643b1d81c>
2003-10-02 19:58:04 +0000

    bump versioning
    git-svn-id: https://tmda.svn.sourceforge.net/svnroot/tmda/trunk@1745 2baf9af7-fe1d-0410-8d70-c10643b1d81c

--- tmda/TMDA/Version.py
+++ tmda/TMDA/Version.py
@@ -26,10 +26,10 @@ import sys
 
 
 # TMDA version
-TMDA = "0.86"
+TMDA = "0.86+"
 
 # TMDA version codename
-CODENAME = "Venetian Way"
+CODENAME = "Carry Back"
 
 # Python version
 PYTHON = sys.version.split()[0]
