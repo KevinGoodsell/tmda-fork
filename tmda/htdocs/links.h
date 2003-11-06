128734f54145dfacea3edfc924ba4acbb1c2e7b9
jasonrm <jasonrm@2baf9af7-fe1d-0410-8d70-c10643b1d81c>
2003-11-06 21:56:13 +0000

    Slight reorganization.
    * Rename 'Overview' to 'About'.
    
    * Move Donations to 'About'.
    
    * Add a Miscellaneous section for items which don't fit
    neatly into other sections.
    
    * Move External Resources and Mirrors to Miscellaneous.
    
    
    git-svn-id: https://tmda.svn.sourceforge.net/svnroot/tmda/trunk@1768 2baf9af7-fe1d-0410-8d70-c10643b1d81c

--- tmda/htdocs/links.h
+++ tmda/htdocs/links.h
@@ -1,5 +1,5 @@
 <!-- -*- html -*- -->
-<h3>Overview</h3>
+<h3>About</h3>
 <li><a href="index.html">Introduction</a>
 <li><a href="history.html">History</a>
 <li><a href="features.html">Features</a>
@@ -7,6 +7,7 @@
 <li><a href="results.html">Results &amp; Testimonials</a>
 <li><a href="inuse.html">TMDA In Use</a>
 <li><a href="press.html">Press Coverage</a>
+<li><a href="donations.html">Donations</a>
 <h3>Install</h3>
 <li><a href="requirements.html">Requirements</a>
 <li><a href="download.html">Download</a>
@@ -32,6 +33,6 @@
 <li><a href="http://tmda.net/lists/listinfo/" TARGET="Resource Window">Mailing Lists</a>
 <li><a href="http://mla.libertine.org/" TARGET="Resource Window">List Archive</a> 
 <li><a href="support-commercial.html">Commercial Support</a>
+<h3>Miscellaneous</h3>
 <li><a href="resources.html">External Resources</a>
 <li><a href="mirrors.html">Mirrors</a>
-<li><a href="donations.html">Donations</a>
