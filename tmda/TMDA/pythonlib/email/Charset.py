7f288a9700dee17e73fbd5b67e8b916ebb9a5b50
jasonrm <jasonrm@2baf9af7-fe1d-0410-8d70-c10643b1d81c>
2003-11-26 19:19:34 +0000

    It turns out that we can support both KoreanCodecs/JapaneseCodecs as well as CJKCodecs.  As {CJK,Japanese,Korean}Codecs have their own .pth file that registers their encoding aliases, there's no need to use a specific module prefix like 'japanese.', 'korean.', or 'cjkcodecs.'. The only incompatibility is with ChineseCodecs.
    git-svn-id: https://tmda.svn.sourceforge.net/svnroot/tmda/trunk@1797 2baf9af7-fe1d-0410-8d70-c10643b1d81c

--- tmda/TMDA/pythonlib/email/Charset.py
+++ tmda/TMDA/pythonlib/email/Charset.py
@@ -94,15 +94,15 @@ ALIASES = {
 # unicode codec set for Chinese, Japanese and Korean encodings.
 
 CODEC_MAP = {
-    'euc-jp':      'cjkcodecs.euc-jp',
-    'iso-2022-jp': 'cjkcodecs.iso-2022-jp',
-    'shift_jis':   'cjkcodecs.shift_jis',
-    'euc-kr':      'cjkcodecs.euc-kr',
-    'ks_c_5601-1987': 'cjkcodecs.cp949',
-    'iso-2022-kr': 'cjkcodecs.iso-2022-kr',
-    'johab':       'cjkcodecs.johab',
-    'gb2312':      'cjkcodecs.gb2312',
-    'big5':        'cjkcodecs.big5',
+    'euc-jp':      'euc-jp',
+    'iso-2022-jp': 'iso-2022-jp',
+    'shift_jis':   'shift_jis',
+    'euc-kr':      'euc-kr',
+    'ks_c_5601-1987': 'cp949',
+    'iso-2022-kr': 'iso-2022-kr',
+    'johab':       'johab',
+    'gb2312':      'gb2312',
+    'big5':        'big5',
     'utf-8':       'utf-8',
     # Hack: We don't want *any* conversion for stuff marked us-ascii, as all
     # sorts of garbage might be sent to us in the guise of 7-bit us-ascii.
