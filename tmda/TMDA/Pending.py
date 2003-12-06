03ad7138747483ac952666370f59c0d97b1a6ec3
tlegant <tlegant@2baf9af7-fe1d-0410-8d70-c10643b1d81c>
2003-12-06 01:31:46 +0000

    Discard last change.  tmda-cgi wants to display MIME parts.
    git-svn-id: https://tmda.svn.sourceforge.net/svnroot/tmda/trunk@1811 2baf9af7-fe1d-0410-8d70-c10643b1d81c

--- tmda/TMDA/Pending.py
+++ tmda/TMDA/Pending.py
@@ -421,7 +421,10 @@ class Message:
         self.msgfile = os.path.join(Defaults.PENDING_DIR, self.msgid)
         if not os.path.exists(self.msgfile):
             raise Errors.MessageError, '%s not found!' % self.msgid
-        self.msgobj = Util.msg_from_file(open(self.msgfile, 'r'))
+        try:
+            self.msgobj = email.message_from_file(open(self.msgfile, 'r'))
+        except email.Errors.BoundaryError:
+            self.msgobj = Util.msg_from_file(open(self.msgfile, 'r'))
         self.recipient = recipient
 
     def initMessage(self, recipient = None):
