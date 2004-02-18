cbbe9f96b0f6da9b5917b4d3867d5d5a100ed23c
lack <lack@2baf9af7-fe1d-0410-8d70-c10643b1d81c>
2004-02-18 17:16:08 +0000

    Now can edit more than just the three hard-coded templates.
    git-svn-id: https://tmda.svn.sourceforge.net/svnroot/tmda/trunk@1940 2baf9af7-fe1d-0410-8d70-c10643b1d81c

--- tmda-cgi/Templates.py
+++ tmda-cgi/Templates.py
@@ -44,50 +44,53 @@ def Show():
   else:
     T["NoSave"]
 
-  for BaseName in ("bounce", "confirm_accept", "confirm_request"):
-    if Defaults.TEMPLATE_DIR:
-      Filename = os.path.join(Defaults.TEMPLATE_DIR, BaseName + ".txt")
-    else:
-      Filename = os.path.join(os.environ["TMDA_BASE_DIR"], "templates",
-        BaseName + ".txt")
-    T["Var"] = BaseName
+  if Defaults.TEMPLATE_DIR:
+    baseDir = Defaults.TEMPLATE_DIR
+  else:
+    baseDir = os.environ["TMDA_BASE_DIR"]
 
-    # Get file
-    T["FilePath"] = Filename
-    if CgiUtil.TestTextFilePath(Filename):
-      try:
-        F = open(Filename)
-        FileContents = F.read()
-        F.close()
-      except IOError:
-        FileContents = ""
-    else:
-      FileContents = "(File is not accessible.)"
+  for BaseName in os.listdir( baseDir ):
+    if BaseName.endswith( ".txt" ):
+      Filename = os.path.join( baseDir, BaseName )
 
-    # Saving?
-    if Form.has_key("subcmd") and \
-      (PVars[("NoOverride", "MayEditTemplates")][0].lower() == "y"):
-      try:
-        # Make sure the list is properly formatted
+      T["Var"] = BaseName
+
+      # Get file
+      T["FilePath"] = Filename
+      if CgiUtil.TestTextFilePath(Filename):
         try:
-          FileContents = re.sub("\r\n", "\n", Form[BaseName].value)
-        except KeyError:
-          FileContents = ""
-        FileContents = re.sub("\n*$", "", FileContents)
-        FileContents += "\n"
-
-        if CgiUtil.TestTextFilePath(Filename):
-          F = open(Filename, "w")
-          F.write(FileContents)
+          F = open(Filename)
+          FileContents = F.read()
           F.close()
-      except IOError, ErrStr:
-        CgiUtil.TermError("Unable to save template file.",
-        "Insufficient privileges", "save template", "%s<br>%s" % (ErrStr,
-        CgiUtil.FileDetails("Template", Filename)),
-        "Change file permissions on <tt>%s</tt>" % Filename)
+        except IOError:
+          FileContents = ""
+      else:
+        FileContents = "(File is not accessible.)"
 
-    T["FileContents"] = FileContents
-    Row.Add()
+      # Saving?
+      if Form.has_key("subcmd") and \
+        (PVars[("NoOverride", "MayEditTemplates")][0].lower() == "y"):
+        try:
+          # Make sure the list is properly formatted
+          try:
+            FileContents = re.sub("\r\n", "\n", Form[BaseName].value)
+          except KeyError:
+            FileContents = ""
+          FileContents = re.sub("\n*$", "", FileContents)
+          FileContents += "\n"
+
+          if CgiUtil.TestTextFilePath(Filename):
+            F = open(Filename, "w")
+            F.write(FileContents)
+            F.close()
+        except IOError, ErrStr:
+          CgiUtil.TermError("Unable to save template file.",
+          "Insufficient privileges", "save template", "%s<br>%s" % (ErrStr,
+          CgiUtil.FileDetails("Template", Filename)),
+          "Change file permissions on <tt>%s</tt>" % Filename)
+
+      T["FileContents"] = FileContents
+      Row.Add()
 
   # Display template
   print T
