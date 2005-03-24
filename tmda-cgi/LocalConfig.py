ab0a28122d483e7ae2059e4303e6536c422b4989
lack <lack@2baf9af7-fe1d-0410-8d70-c10643b1d81c>
2005-03-24 15:49:14 +0000

    1.1.x compatibility fix I added long ago actually should work now...
    git-svn-id: https://tmda.svn.sourceforge.net/svnroot/tmda/trunk@2009 2baf9af7-fe1d-0410-8d70-c10643b1d81c

--- tmda-cgi/LocalConfig.py
+++ tmda-cgi/LocalConfig.py
@@ -139,7 +139,7 @@ def Show():
     FileContents = []
 
   # Which view does the user want?  Form or text?
-  if PVars["LocalConfig"] == "Form" and Version.TMDA < 1.1:
+  if PVars["LocalConfig"] == "Form" and Version.TMDA < "1.1.0":
     # User wants to view the config in form mode.  First we need to dismantle
     # the current config file in a way that we can rebuild it, then we need to
     # verify that it is safe to proceed.
@@ -256,7 +256,7 @@ Not safe to proceed in form mode.
   else:
     FileContents = "".join(FileContents)
 
-  if PVars["LocalConfig"] == "Text" or Version.TMDA >= 1.1:
+  if PVars["LocalConfig"] == "Text" or Version.TMDA >= "1.1.0":
     # We're in text view, hide form view
     T["FormView"]
     T["FileContents"] = FileContents
