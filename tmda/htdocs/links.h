0d1d584737a31dfb0bd8fe71fa267a768a98a353
jasonrm <jasonrm@2baf9af7-fe1d-0410-8d70-c10643b1d81c>
2006-09-17 01:47:18 +0000

    The TMDA FAQ is now located within the Wiki at http://wiki.tmda.net/TmdaFaq
    git-svn-id: https://tmda.svn.sourceforge.net/svnroot/tmda/trunk@2015 2baf9af7-fe1d-0410-8d70-c10643b1d81c

--- tmda/htdocs/links.h
+++ tmda/htdocs/links.h
@@ -6,6 +6,6 @@
 <li><a href="http://wiki.tmda.net/TmdaDownload">Download</a>
 <li><a href="http://wiki.tmda.net/TmdaDocumentation">Documentation</a>
 <li><a href="http://wiki.tmda.net/TmdaSupport">Support</a>
-<li><a href="http://tmda.net/faq.cgi">FAQ</a>
+<li><a href="http://wiki.tmda.net/TmdaFaq">FAQ</a>
 <li><a href="http://wiki.tmda.net/TmdaDonations">Donations</a>
 
