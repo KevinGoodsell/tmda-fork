30f94f911a30ed28d9c258eeecedaf7327929644
lack <lack@2baf9af7-fe1d-0410-8d70-c10643b1d81c>
2006-10-16 14:12:55 +0000

    Version number bumps, ready for next release
    git-svn-id: https://tmda.svn.sourceforge.net/svnroot/tmda/trunk@2112 2baf9af7-fe1d-0410-8d70-c10643b1d81c

--- tmda-cgi/CgiVersion.py
+++ tmda-cgi/CgiVersion.py
@@ -24,10 +24,10 @@
 from TMDA import Version
 
 # tmda-cgi version
-tmda_cgi = "0.14"
+tmda_cgi = "0.15"
 
 # tmda-cgi version codename
-Codename = "Silicon"
+Codename = "Phosphorus"
 
 # TMDA version required
 TMDAReqVer = "1.1.6"
