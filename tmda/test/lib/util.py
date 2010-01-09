35402cd32f89c02e773f4a0ebd1904a1d386a0e5
Kevin Goodsell <kevin-opensource@omegacrash.net>
2010-01-09 00:31:25 -0800

    Add tests for Cookie.py, update test utils.

--- tmda/test/lib/util.py
+++ tmda/test/lib/util.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 rootDir = '..'
 userDir = os.path.join('home', 'testuser')
@@ -11,3 +12,14 @@ def fixupFiles():
 
     os.chmod(os.path.join(userDir, '.tmda', 'crypt_key'), 0600)
     os.chmod(os.path.join(filesDir, 'test-ofmipd.auth'), 0600)
+
+def fixupPythonPath():
+    sys.path.append(rootDir)
+
+def fixupHome():
+    os.environ['HOME'] = userDir
+
+def testPrep():
+    fixupFiles()
+    fixupHome()
+    fixupPythonPath()
